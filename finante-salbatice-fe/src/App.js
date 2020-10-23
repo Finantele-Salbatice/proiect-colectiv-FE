b13b37cbc76f39dc564d37e84b4feaa86f37033a
iuliusavgvstvs <iulius.satmari@gmail.com>
2020-10-23 22:13:13 +0300

    daw

--- finante-salbatice-fe/src/App.js
+++ finante-salbatice-fe/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dragos Gay
+          Dragos Gay gay gay gay gay
         </p>
         <a
           className="App-link"
