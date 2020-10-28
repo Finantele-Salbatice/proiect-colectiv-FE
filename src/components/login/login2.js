394f1bf440cd62c82ed35e17900829f6e0697d66
Dragos1221 <48069261+Dragos1221@users.noreply.github.com>
2020-10-28 11:56:55 +0200

    Dragos

--- src/components/login/login2.js
+++ src/components/login/login2.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+import {Button,TextField} from "@material-ui/core"
+import "../../css/login/main.css"
+import "../../css/login/util.css"
+
 class Login2 extends Component {
     constructor(props)
     {
@@ -38,41 +42,50 @@ class Login2 extends Component {
     render() {
         const {email, password} = this.state;
         return (
-            <div className="Login">
-              <form>
-                <div controlId="email" bsSize="large">
-                  <label>Email</label>
-                  <input
-                    autoFocus
-                    type="email"
-                    value={email}
-                    onChange={e => this.setEmail(e.target.value)}
-                  />
-                </div>
-                <div controlId="password" bsSize="large">
-                  <label>Password</label>
-                  <input
-                    value={password}
-                    onChange={e => this.setPassword(e.target.value)}
-                    type="password"
-                  />
-                </div>
-                <div className="buttonLogin">
-                    <button block bsSize="large" type="submit" onClick={() =>this.finisj()}>
-                    Login
-                    </button>
-                </div>
-                <div class="forgotPass">
-                        <a class="txt1" href="#">
-                            Forgot Password?
-                        </a>
+            
+            <div class="limiter">
+                <div class="container-login100">
+                    <div class="wrap-login100">
+                        <form class="login100-form validate-form">
+                            <span class="login100-form-logo">
+                                <i class="zmdi zmdi-landscape"></i>
+                            </span>
+        
+                            <span class="login100-form-title p-b-34 p-t-27">
+                                Log in
+                            </span>
+        
+                            <div class="wrap-input100 validate-input" data-validate = "Enter username">
+                                <input class="input100" type="text" name="username" placeholder="Username"/>
+                                <span class="focus-input100" data-placeholder="&#xf207;"></span>
+                            </div>
+        
+                            <div class="wrap-input100 validate-input" data-validate="Enter password">
+                                <input class="input100" type="password" name="pass" placeholder="Password"/>
+                                <span class="focus-input100" data-placeholder="&#xf191;"></span>
+                            </div>
+        
+                            <div class="contact100-form-checkbox">
+                                <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me"/>
+                                <label class="label-checkbox100" for="ckb1">
+                                    Remember me
+                                </label>
+                            </div>
+        
+                            <div class="container-login100-form-btn">
+                                <button class="login100-form-btn">
+                                    Login
+                                </button>
+                            </div>
+        
+                            <div class="text-center p-t-90">
+                                <a class="txt1" href="#">
+                                    Forgot Password?
+                                </a>
+                            </div>
+                        </form>
                     </div>
-                <div class="forgotPass">
-                        <a class="txt1" href="#">
-                            Sing in.
-                        </a>
-                    </div>
-              </form>
+                </div>
             </div>
         );
     }
