cb55703cfa1f6bfbe7524390b4abcc165de6c281
andragabrielapufu99 <62243700+andragabrielapufu99@users.noreply.github.com>
2020-11-12 22:08:10 +0200

    Andra (#23)
    forgot and reset password

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegsiterPage';
+import ResetPassPage from './pages/ResetPassPage';
+import ChangePassPage from './pages/ChangePassPage';
 import React from 'react';
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact={true} path="/" component={LoginPage}></Route>
-        <Route exact={true} path="/register" component={RegisterPage}></Route>
-      </Switch>
-    </Router>
-  );
+function app() {
+	return (
+		<Router>
+			<Switch>
+				<Route exact={true} path="/" component={LoginPage}></Route>
+				<Route exact={true} path="/register" component={RegisterPage}></Route>
+				<Route exact={true} path="/reset" component={ResetPassPage} />
+				<Route exact={true} path="/reset/:token" component={ChangePassPage} />
+			</Switch>
+		</Router>
+	);
 }
 
-export default App;
+export default app;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,72 +2,74 @@ import * as React from 'react';
 import Login from '../components/login/Login';
 import axios, { AxiosInstance } from 'axios';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
 const { SNOWPACK_PUBLIC_API_URL } = import.meta.env;
 
 interface LoginPageProps extends RouteComponentProps {}
 
 interface LoginPageState {
-  password: string;
-  email: string;
-  isError: boolean;
-  errorMessage: string;
+	password: string;
+	email: string;
+	isError: boolean;
+	errorMessage: string;
 }
 
 class LoginPage extends React.Component<LoginPageProps, LoginPageState> {
-  instance: AxiosInstance;
-  constructor(props: LoginPageProps) {
-    super(props);
-    this.state = {
-      password: '',
-      email: '',
-      isError: false,
-      errorMessage: 'Parola nu este corecta.',
-    };
-    this.instance = axios.create({
-      baseURL: SNOWPACK_PUBLIC_API_URL,
-    });
-  }
+	instance: AxiosInstance;
+	constructor(props: LoginPageProps) {
+		super(props);
+		this.state = {
+			password: '',
+			email: '',
+			isError: false,
+			errorMessage: 'Parola nu este corecta.',
+		};
+		this.instance = axios.create({
+			baseURL: SNOWPACK_PUBLIC_API_URL,
+		});
+	}
 
-  handleChange = (data: any) => {
-    this.setState({
-      ...data,
-      isError: false,
-    });
-  };
-  clearUserData() {
-    this.setState({
-      password: '',
-      isError: true,
-    });
-  }
+	handleChange = (data: any) => {
+		this.setState({
+			...data,
+			isError: false,
+		});
+	};
+	clearUserData() {
+		this.setState({
+			password: '',
+			isError: true,
+		});
+	}
 
-  submit = async () => {
-    try {
-      const result = await this.instance.post('/login', this.state);
-      const data = result.data;
-      console.log('E bine,', data);
-      this.props.history.push('/reset');
-    } catch (error) {
-      const { response } = error;
-      console.log(response.data); // make some text appear if this error is received
-      this.setState({
-        isError: true,
-      });
-      this.clearUserData();
-    }
-  };
+	submit = async() => {
+		try {
+			const result = await this.instance.post('/login', this.state);
+			const data = result.data;
+			console.log('E bine,', data);
+			this.props.history.push('/reset');
+		} catch (error) {
+			const { response } = error;
+			console.log(response.data); // make some text appear if this error is received
+			this.setState({
+				isError: true,
+			});
+			this.clearUserData();
+		}
+	};
 
-  render() {
-    return (
-      <div>
-        <Login
-          {...this.state}
-          handleChange={this.handleChange}
-          submit={this.submit}
-        ></Login>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Login
+					{...this.state}
+					handleChange={this.handleChange}
+					submit={this.submit}
+				></Login>
+			</div>
+		);
+	}
 }
 
 export default withRouter(LoginPage);
