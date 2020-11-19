7d572dc3e8dadce061de4649b1a470e87c8cb768
Dragos1221 <48069261+Dragos1221@users.noreply.github.com>
2020-11-19 10:35:13 +0200

    NavBar and AppBar (#26)
     navbar and sidebar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,54 @@
+import { createStyles, Divider, Drawer, ListItem, ListItemIcon, ListItemText, withStyles } from '@material-ui/core';
+import * as React from 'react';
+import List from '@material-ui/core/List';
+import AccountBalanceTwoToneIcon from '@material-ui/icons/AccountBalanceTwoTone';
+import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+
+export interface NavBarProps {
+    classes: any;
+}
+export interface NavBarState {
+}
+
+const styles = createStyles({
+	drawer: {
+		width: '240px',
+		flexShrink: 0,
+	},
+	drawerBox : {
+		display:'inline-block',
+	},
+});
+
+class NavBar extends React.Component<NavBarProps, NavBarState> {
+    state = {
+    }
+
+    render() {
+    	const { classes } = this.props;
+    	return (
+    		<div className={classes.drawerBox}>
+    			<Drawer
+    				variant="permanent"
+    			>
+    				<List>
+    					<ListItem button key={1}>
+    						<ListItemIcon> <AccountBalanceTwoToneIcon /></ListItemIcon>
+    						<ListItemText primary={'Home'} />
+    					</ListItem>
+    					<ListItem button key={2}>
+    						<ListItemIcon> <AccountBalanceWalletIcon /></ListItemIcon>
+    						<ListItemText primary={'Tranzactii'} />
+    					</ListItem>
+    					<ListItem button key={3}>
+    						<ListItemIcon> <AccountBoxIcon /></ListItemIcon>
+    						<ListItemText primary={'Conturi'} />
+    					</ListItem>
+    				</List>
+    			</Drawer>
+    		</div>
+    	);
+    }
+}
+export default withStyles(styles)(NavBar);
