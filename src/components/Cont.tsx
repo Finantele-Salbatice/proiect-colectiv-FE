6f8aa01852bd10a2e1eb5862a7168852200f3ed2
Dragos1221 <48069261+Dragos1221@users.noreply.github.com>
2020-11-24 20:49:58 +0200

    Cont Component

---
+++ src/components/Cont.tsx
@@ -0,0 +1,51 @@
+import { Card, CardContent, CardMedia, createStyles, Typography, withStyles } from '@material-ui/core';
+import * as React from 'react';
+import { Component } from 'react';
+
+export interface ContProps {
+    sold: number;
+    banca: string;
+	iban: string;
+	descriere: string;
+    classes: any;
+}
+
+export interface ContState {
+}
+
+const styles = createStyles({
+	root: {
+		display: 'flex',
+	},
+	image:{
+		width:'140px',
+		height:'140px',
+	},
+});
+
+class Cont extends React.Component<ContProps, ContState> {
+	render() {
+		const { classes } = this.props;
+		return (
+			<Card className={classes.root}>
+				<div>
+					<img src='bt.jpg' className={classes.image}></img>
+				</div>
+				<CardContent>
+					<Typography component="h5" variant="h5">
+                            IBAN:{this.props.iban}
+					</Typography>
+					<Typography component="h5" variant="h5">
+                            Sold:{this.props.sold}
+					</Typography>
+					<Typography component="h5" variant="h5">
+                            Descirere:{this.props.descriere}
+					</Typography>
+				</CardContent>
+			</Card>
+
+		);
+	}
+}
+
+export default withStyles(styles)(Cont);
